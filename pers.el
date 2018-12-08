ea4e27acf5a1865363306200423e5c8f185a10c0
Patryk Matuszak <patryk.mtk@gmail.com>
2018-12-08 22:34:50 +0100

    nlinum-relative

--- pers.el
+++ pers.el
@@ -4,5 +4,15 @@
 (define-key rust-mode-map (kbd "TAB") #'company-indent-or-complete-common)
 (setq company-tooltip-align-annotations t)
 
+(with-eval-after-load 'rust-mode
+  (add-hook 'flycheck-mode-hook #'flycheck-rust-setup))
+
 (define-key evil-motion-state-map ";" 'evil-ex)
 (define-key evil-motion-state-map ":" 'evil-repeat-find-char)
+
+(prelude-require-package 'nlinum-relative)
+(nlinum-relative-setup-evil)
+(add-hook 'prog-mode-hook 'nlinum-relative-mode)
+(setq nlinum-relative-redisplay-delay 0)
+(setq nlinum-relative-current-symbol "->")
+(setq nlinum-relative-offset 0)
